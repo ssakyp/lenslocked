7936556c259031e11b443f955ea69c12fe2f745f
Kamilya Kydykova <sultansakyp77@gmail.com>
2024-10-04 11:20:22 +0500

    contact

--- main.go
+++ main.go
@@ -7,18 +7,23 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"time"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "<h1>Welcome to my awesome site!<h1>")
 
 }
 
-func main() {
-	http.HandleFunc("/", handlerFunc)
-	time.Sleep(3 * time.Second)
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<h1>Contact Page</h1><p>To get in touch, email me at <a 
+	href="mailto:sultansakyp77@gmail.com">sultansakyp77@gmail.com</a>.</p>`)
+}
 
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/contact", contactHandler)
 	fmt.Println("Starting the server on :3000... ")
 	http.ListenAndServe(":3000", nil)
 
