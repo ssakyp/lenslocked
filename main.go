fbcb07c1a19d16b55a031ba6990437e8bf146672
Kamilya Kydykova <sultansakyp77@gmail.com>
2024-10-05 18:54:49 +0500

    faq page

--- main.go
+++ main.go
@@ -21,25 +21,54 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 	href="mailto:sultansakyp77@gmail.com">sultansakyp77@gmail.com</a>.</p>`)
 }
 
-func pathHandler(w http.ResponseWriter, r *http.Request) {
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, `<h1>
+	FAQ Page
+	</h1>
+	<p>
+	Q: Is there a free version?
+	<br />
+	A: Yes! We offer a free trial for 30 days on any paid plans.
+	</p>
+	<p>
+	Q: What are your support hours?
+	<br />
+	A: We have support staff answering emails 24/7, though response times may be a bit slower on weekends.
+	</p>
+	<p>
+	Q: How do I contact support?
+	<br />
+	A: Email us - <ahref="mailto:sultansakyp77@gmail.com">sultansakyp77@gmail.com</a>
+	</p>
+	
+	`)
+}
+
+type Router struct{}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.URL.Path {
 	case "/":
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		http.Error(w, "Page not found", http.StatusNotFound)
-		//http.StatusText(http.StatusNotFound)
-		// w.WriteHeader(404)
-		// fmt.Fprint(w, "Page not found")
 	}
 }
 
 func main() {
-	http.HandleFunc("/", pathHandler)
+	var router Router
+
+	// http.HandleFunc("/", pathHandler)
 	// http.HandleFunc("/", homeHandler)
 	// http.HandleFunc("/contact", contactHandler)
+	//var router http.HandlerFunc = pathHandler
+
 	fmt.Println("Starting the server on :3000... ")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", router)
 
 }
